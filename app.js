2d906f77d8281f8ff81bf820b6ce13ca1dd69c2f
madhushrabafm <madhushraba.das@fortmindz.in>
2024-07-25 18:09:28 +0530

    backend rendering all users on joining

---
+++ app.js
@@ -0,0 +1,55 @@
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const server = createServer(app);
+
+const io = new Server(server);
+
+const userSocketMap = {};
+// {
+//   "random room id vmirvnrkvnrekjfre" : "username",
+//   "randomid 2 KEY " : "username 2 VALUE",
+// } in this format data will be stored in userSocketMap
+
+const getAllUsers = (roid) => {
+  return Array.from(io.sockets.adapter.rooms.get(roid) || []).map(
+    (socket_id) => {
+      return {
+        socket_id,
+        username: userSocketMap[socket_id],
+      };
+    }
+  );
+};
+
+io.on("connection", (socket) => {
+  console.log("socket connection", socket.id);
+
+  // receivieng from frontend
+  socket.on("joinRoom", ({ roomid, username }) => {
+    // storing the username as KEY  in userSocketMap
+    userSocketMap[socket.id] = username;
+    socket.join(roomid); //entering the new user in the defined roomid
+    const allUsers = getAllUsers(roomid); //getting all users from the room
+    console.log(allUsers);
+    // send notif to other users when new user joins
+    allUsers.forEach(({ socket_id }) => {
+      //when new user joins room
+      io.to(socket_id).emit("joined", {
+        allUsers,
+        username,
+        socketid: socket.id,
+      });
+    });
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("madhua");
+});
+
+server.listen(3333, () => {
+  console.log("server onn d >>>>>>>>>>>>>>>");
+});
